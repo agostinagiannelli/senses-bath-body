7923815d30960bb7cb05539ffb53909806ee9e20
Agostina Giannelli <agostinagiannelli@gmail.com>
2023-02-17 11:28:07 +0100

    Added reduce, forEach and update prompts

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const productList = [
+const products = [
     { sku: 1491605, variant: "Aloha Monoi", product: "Body Mist", regularPrice: 6, size: "100ml" },
     { sku: 1460959, variant: "Aloha Monoi", product: "Bubble Bath", regularPrice: 11, size: "1lt" },
     { sku: 1463555, variant: "Aloha Monoi", product: "Shower Gel", regularPrice: 9, size: "500ml" },
@@ -20,21 +20,14 @@ let flagVariant = true;
 let flagQuantity = true;
 let flagAdd = true;
 let flagShipping = true;
-let addProduct;
-let productType;
-let variantType;
-let quantity;
-let productAddToCart;
-let shipping;
-let shippingCost = 0;
 
 let customerName = prompt("Bienvenido :) Por favor, ingresá tu nombre.");
 
 while (flagAdd) {
-    addProduct = prompt(`${customerName}, por favor ingresá aceptar si deseás agregar un producto al carrito o rechazar si deseas continuar. Si deseás salir podés ingresar cancelar.`);
-    if (addProduct === "aceptar") {
+    addProduct = prompt(`${customerName}, por favor ingresá AGREGAR para sumar un producto al carrito, CONTINUAR para avanzar al siguiente paso o CANCELAR para salir.`).toLowerCase();
+    if (addProduct === "agregar") {
         fnProduct();
-    } else if (addProduct === "rechazar") {
+    } else if (addProduct === "continuar") {
         fnShipping();
     } else if (addProduct === "cancelar") {
         flagAdd = false;
@@ -43,7 +36,10 @@ while (flagAdd) {
     }
 
     function fnProduct() {
-        productType = prompt("Elegí uno de los siguientes productos: bruma corporal, baño de burbujas o gel de ducha. Si deseás salir ingresá cancelar.").toLowerCase();
+        productType = prompt(`Elegí uno de los siguientes productos o CANCELAR para salir:
+        BRUMA CORPORAL
+        BAÑO DE BURBUJAS
+        GEL DE DUCHA`).toLowerCase();
         while (flagProduct) {
             if (productType === "bruma corporal") {
                 product = "Body Mist";
@@ -65,13 +61,20 @@ while (flagAdd) {
                 flagProduct = false;
             } else {
                 alert(`Ouch :( "${productType}" no es válido, por favor elegí otro.`);
-                productType = prompt("Elegí uno de los siguientes productos: bruma corporal, baño de burbujas o gel de ducha. Si deseás salir ingresá cancelar.").toLowerCase();
+                productType = prompt(`Elegí uno de los siguientes productos o CANCELAR para salir:
+                BRUMA CORPORAL
+                BAÑO DE BURBUJAS
+                GEL DE DUCHA`).toLowerCase();
             }
         }
     }
 
     function fnVariant() {
-        variantType = prompt("Ahora seleccioná la fragrancia: coco, tropical, lavanda o vainilla. Si deseás salir ingresá cancelar.").toLowerCase();
+        variantType = prompt(`Ahora seleccioná la fragancia o CANCELAR para salir:
+        COCO
+        TROPICAL
+        LAVANDA
+        VAINILLA`).toLowerCase();
         while (flagVariant) {
             if (variantType === "coco") {
                 variant = "Aloha Monoi";
@@ -83,7 +86,7 @@ while (flagAdd) {
                 break;
             } else if (variantType === "lavanda") {
                 variant = "Lavender Calm";
-                funcQuantity();
+                fnQuantity();
                 break;
             } else if (variantType === "vainilla") {
                 variant = "Simply Luxurious";
@@ -93,13 +96,17 @@ while (flagAdd) {
                 flagVariant = false;
             } else {
                 alert(`Ouch :( "${variantType}" no es válida, por favor elegí otra.`);
-                variantType = prompt("Seleccioná la fragrancia: coco, tropical, lavanda o vainilla. Si deseás salir ingresá cancelar.").toLowerCase();
+                variantType = prompt(`Ahora seleccioná la fragancia o CANCELAR para salir:
+                COCO
+                TROPICAL
+                LAVANDA
+                VAINILLA`).toLowerCase();
             }
         }
     }
 
     function fnQuantity() {
-        quantity = parseInt(prompt("Ingresá la cantidad de unidades. Para salir ingresá 0."));
+        quantity = parseInt(prompt("Ingresá la cantidad de unidades o 0 para salir."));
         while (flagQuantity) {
             if (quantity > 0) {
                 fnAddToCart();
@@ -108,48 +115,49 @@ while (flagAdd) {
                 flagQuantity = false;
             } else {
                 alert("Por favor, ingresá una cantidad válida.");
-                quantity = parseInt(prompt("Ingresá la cantidad de unidades. Para salir ingresá 0."));
+                quantity = parseInt(prompt("Ingresá la cantidad de unidades o 0 para salir."));
             }
         }
     }
 
     function fnAddToCart() {
-        const addToCart = productList.find((item) => item.product === product && item.variant === variant);
-        alert(`Súper, excelente elección: ${quantity}x ${addToCart.variant} ${addToCart.product} - ${addToCart.regularPrice}€ cada uno.`);
-        productAddToCart = { sku: addToCart.sku, variant: addToCart.variant, product: addToCart.product, regularPrice: addToCart.regularPrice };
-        cart.push(productAddToCart);
-        alert(cart);
+        const addToCart = products.find((addToCart) => addToCart.product === product && addToCart.variant === variant);
+        alert(`Súper, excelente elección, se agregó al carrito ${quantity}x ${addToCart.variant} ${addToCart.product} a ${addToCart.regularPrice}€ cada uno.`);
+        productAddedToCart = { sku: addToCart.sku, variant: addToCart.variant, product: addToCart.product, regularPrice: addToCart.regularPrice, quantity: quantity };
+        cart.push(productAddedToCart);
     }
 }
 
-/* function fnShipping() {
-    shipping = prompt("¿El envío es local o internacional? Podés ingresar cancelar si deseás salir.").toLowerCase();
+function fnShipping() {
+    shipping = prompt("¿El envío es LOCAL o INTERNACIONAL? Podés ingresar CANCELAR si deseás salir.").toLowerCase();
     while (flagShipping) {
         if (shipping === "local") {
             shippingCost = 10;
-            alert(`Tu costo de envío será de ${shippingCost}€. A continuación verás el resumen de tu pedido.`);
-            fnTotalCost();
+            fnCheckout();
             break;
         } else if (shipping === "internacional") {
             shippingCost = 50;
-            alert(`Tu costo de envío será de ${shippingCost}€. A continuación verás el resumen de tu pedido.`);
-            fnTotalCost();
+            fnCheckout();
             break;
         } else if (shipping == "cancelar") {
             flagShipping = false;
         } else {
             alert("Por favor, ingresá un tipo de envío válido.");
-            shipping = prompt("¿El envío es local o internacional? Podés ingresar cancelar si deseás salir.").toLowerCase();
+            shipping = prompt("¿El envío es LOCAL o INTERNACIONAL? Podés ingresar CANCELAR si deseás salir.").toLowerCase();
         }
     }
-} */
+}
 
-/* // arrow function para calcular el costo total desde navegador
-
-function fnTotalCost() {
-    const add = (a, b) => a + b;
-    const multiply = (a, b) => a * b;
-    let totalCost = add(multiply(regularPrice, quantity), shippingCost);
-    let summary = `¡Muchas gracias, ${customerName}! Este es el resumen de tu pedido: ${quantity}x ${variant} ${product} con envío ${shipping}. El costo total será de ${totalCost}€.`;
+function fnCheckout() {
+    let totalProducts = cart.reduce((acum, item) => acum + item.regularPrice * item.quantity, 0);
+    let totalAmount = totalProducts + shippingCost
+    let summary = `¡Muchas gracias, ${customerName}! Este es el resumen de tu pedido:
+    Envío ${shipping}: ${shippingCost}€
+    Costo total: ${totalAmount}€`;
+    cart.forEach((item) => {
+        summary += `
+        ${item.quantity}x ${item.variant} ${item.product} - Unidad: ${item.regularPrice}€ Total: ` + item.regularPrice * item.quantity + `€`
+    });
     alert(summary);
-} */
+    flagAdd = false;
+}
